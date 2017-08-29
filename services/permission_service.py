6f04cafcde8bf2b3b27ac4a7728cc8fd0855f45c
StevenXue <15994039246@163.com>
2017-08-29 16:36:52 +0800

    pservice updated

--- services/permission_service.py
+++ services/permission_service.py
@@ -1,22 +1,15 @@
 # -*- coding: UTF-8 -*-
-from business.permission_business import Permission,PermissionBusiness
-from business.role_business import Role
-from database.config_setting import db, date_now
+from business.permission_business import Permission, PermissionBusiness
+from business.resource_business import ResourceBusiness
+from business.role_business import RoleBusiness
+from database.config_setting import db, date_time
+
 
 class PermissionService(object):
     def __init__(self):
         '''
         Constructor
         '''
-
-    # 查询出所有的权限列表
-    @classmethod
-    def find_all_permission(self):
-        perms = Permission.query().all()
-        for re in perms:
-            if re is not None:
-                yield re
-        pass
 
     '''
      * 根据用户角色获取该用户的权限
@@ -26,24 +19,38 @@ class PermissionService(object):
 
     @classmethod
     def find_perm_by_role(self, role_name):
-        role = Role.query().filter(name=role_name).one()
+        role = RoleBusiness.find_by_role_name(role_name)
         for perm in role.perms:
             if perm is not None:
                 yield perm
         pass
 
     '''
+     * 根据资源获取该用户的权限
+     * @param role_name
+     * @return perm set
+    '''
+    @classmethod
+    def find_perm_by_resource(self, res_name):
+        res = ResourceBusiness.find_by_name(res_name)
+        for perm in res.perms:
+            if perm is not None:
+                yield perm
+        pass
+
+
+    '''
     * 根据权限名称获取用户角色
     * @param perm_name
     * @return role set
     '''
-
     @classmethod
     def find_roles_by_perm(self, perm_name):
-        perm = Permission.query().filter(name=perm_name).one()
-        for role in perm.roles:
-            if role is not None:
-                yield role
+        perm = PermissionBusiness.find_by_name(perm_name)
+        if perm is not None:
+            for role in perm.roles:
+                if role is not None:
+                    yield role
         pass
 
     '''
@@ -54,8 +61,8 @@ class PermissionService(object):
 
     @classmethod
     def add_permission_by_name(self, perm_name):
-        print(u'类中的时间: ', date_now)
-        perm = Permission(name=str(perm_name), o_type='2',create_by='Admin',content='TODO ')
+        print(u'类中的时间: ', date_time)
+        perm = Permission(name=str(perm_name), o_type='2', create_by='Admin', content='TODO ')
         PermissionBusiness.add_permission(perm)
 
     '''
@@ -67,6 +74,39 @@ class PermissionService(object):
     @classmethod
     def delete_permission_by_name(self, perm_name):
 
-        perm = db.session.query(Permission).filter_by(name=perm_name).first()
+        perm = PermissionBusiness.find_by_name(perm_name)
         if perm is not None:
             PermissionBusiness.delete_permission(perm)
+
+    '''
+    * 根据名稱添加用户的权限
+    * @param role_name
+    * @return role
+    '''
+    @classmethod
+    def add_permission_by_role_name(cls, perm_name, role_name):
+        perm = PermissionBusiness.find_by_name(perm_name)
+        role = RoleBusiness.find_by_role_name(role_name)
+        if role is not None and perm is not None:
+            role.perms.append(perm)
+            print (role.name +'--添加--'+perm.name +'--权限成功！')
+        db.session.commit()
+        pass
+
+
+    '''
+    * 根据名稱移除角色的权限
+    * @param role_name,perm_name
+    * @return 
+    '''
+    @classmethod
+    def remove_permission_by_name(cls, perm_name, role_name):
+        perm = PermissionBusiness.find_by_name(perm_name)
+        role = RoleBusiness.find_by_role_name(role_name)
+        if role is not None and perm is not None:
+            if perm in role.perms:
+                role.perms.remove(perm)
+                role.modified_date = date_time
+            print(role.name + '--移除--' + perm.name + '--权限成功！')
+            db.session.commit()
+
--- services/role_service.py
+++ services/role_service.py
@@ -4,27 +4,73 @@ from business.user_business import UserBusiness
 from business.role_business import RoleBusiness
 from business.user_group_business import UsergroupBusiness
 
+
 class RoleService(object):
-	
-	def __init__(self):
-		'''
-		Constructor
-		'''
-		
-	# 根据用户角色名查找有哪些用户
-	# @param role_name
-	# @return users
-	def find_users_by_rolename(self, role_name):
-	
-		pass
-	
-	''' 
-	* 根据用户名查找用户拥有的角色信息
-	* 根据角色id 刪除角色
-	* @param role_id
-	* @return role
-	'''
-	def add_role_by_name(self,role_id):
-		
-		pass
-	
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+    # 根据用户角色名查找有哪些用户
+    # @param role_name
+    # @return
+    @classmethod
+    def find_users_by_role_name(cls, role_name):
+        role = RoleBusiness.find_by_role_name(role_name)
+
+        for user in role.users:
+            yield user
+        pass
+
+    ''' 
+    * 根据用户名查找用户拥有的角色信息
+    * 根据角色id 刪除角色
+    * @param role_id
+    * @return role
+    '''
+
+    @classmethod
+    def add_role_by_name(self, role_name):
+
+        pass
+
+    '''
+     * 根据用户的Id查找用户的角色信息
+     * @param userId
+     * @return
+    '''
+    @classmethod
+    def find_role_by_user_id(cls, user_id):
+        user =  UserBusiness.find_user_by_id(user_id)
+        if user is not None:
+            for role in user.roles:
+                yield role
+        pass
+
+    ''' 
+     * 根据用户名查找用户拥有的角色信息
+     * @param username
+     * @return Set<Role>
+    '''
+    @classmethod
+    def find_role_by_user_name(cls, user_name):
+        user = UserBusiness.find_user_by_name(user_name)
+        if user is not None:
+            for role in user.roles:
+                yield role
+        pass
+
+
+    ''' 
+     * 根据角色名查找资源信息（权限）
+     * @param role_name
+     * @return 
+    '''
+
+    @classmethod
+    def find_resource_by_user_name(cls, user_name):
+        user = UserBusiness.find_user_by_name(user_name)
+        if user is not None:
+            for role in user.perms.resources:
+                yield role
+        pass
